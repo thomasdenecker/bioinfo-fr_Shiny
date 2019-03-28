bb911d67d28c76bb25767ad243f455c7fa9fff2b
thomasdenecker <thomas.denecker@gmail.com>
2019-03-28 11:32:46 +0100

    Add graphics

--- app.R
+++ app.R
@@ -15,6 +15,7 @@ library(shiny)
 library(shinydashboard)
 library(DT)
 library(plotly)
+library(ggplot2)
 library(shinyWidgets)
 library(colourpicker)
 library(shinyjs)
@@ -33,6 +34,12 @@ ui <- dashboardPage(
     )
   ),
   dashboardBody(
+    tags$head(tags$style(HTML('
+      .content-wrapper{
+        background-color: white !important;
+      }
+    '))),
+              
     tabItems(
       # Read data
       tabItem(tabName = "readData",
@@ -83,7 +90,17 @@ ui <- dashboardPage(
       
       # visualization
       tabItem(tabName = "visualization",
-              h1("Visualisation des données")
+              h1("Visualisation des données"),
+              h2("Exploration du tableau"),
+              dataTableOutput('dataTable'),
+              h2("Graphiques"),
+              fluidRow(
+                column(3, plotOutput("plotAvecR")),
+                column(3, plotOutput("plotAvecGgplot2")),
+                column(3, plotlyOutput("plotAvecPlotly")),
+                column(3, htmlOutput("plotAvecGoogle"))
+              )
+              
       )
     )
   )
@@ -121,8 +138,7 @@ server <- function(input, output, session) {
       data$table = read.csv(input$dataFile$datapath,
                             header = as.logical(input$header),
                             sep = input$sep,
-                            quote = input$quote,
-                            nrows=10)
+                            quote = input$quote)
       sendSweetAlert(
         session = session,
         title = "Done !",
@@ -134,6 +150,83 @@ server <- function(input, output, session) {
     }
     
   })
+  
+  #=============================================================================
+  # Exploration du tableau
+  #=============================================================================
+  
+  output$dataTable = DT::renderDataTable({
+    datatable(data$table, filter = 'top') %>% 
+      formatStyle('Sepal.Length', 
+                  background = styleColorBar(data$table$Sepal.Length, 'lightcoral'),
+                  backgroundSize = '100% 90%',
+                  backgroundRepeat = 'no-repeat',
+                  backgroundPosition = 'center'
+      ) %>%
+      formatStyle(
+        'Sepal.Width',
+        backgroundColor = styleInterval(c(3,4), c('white', 'red', "firebrick")),
+        color = styleInterval(c(3,4), c('black', 'white', "white"))
+      ) %>%
+      formatStyle(
+        'Petal.Length',
+        background = styleColorBar(data$table$Petal.Length, 'lightcoral'),
+        backgroundSize = '100% 90%',
+        backgroundRepeat = 'no-repeat',
+        backgroundPosition = 'center'
+      ) %>%
+      formatStyle(
+        'Petal.Width',
+        backgroundColor = styleInterval(c(1,2), c('white', 'red', "firebrick")),
+        color = styleInterval(c(1,2), c('black', 'white', "white"))
+      ) %>%
+      formatStyle(
+        'Species',
+        backgroundColor = styleEqual(
+          unique(data$table$Species), c('lightblue', 'lightgreen', 'lavender')
+        )
+      )
+    
+  })
+  
+  output$plotAvecR <- renderPlot({
+    plot(data$table$Petal.Length,data$table$Sepal.Length, 
+         main = "Sepal length vs Petal length (R)",
+         ylab = "Sepal length",
+         xlab = "Petal length")
+  })
+  
+  output$plotAvecGgplot2 <- renderPlot({
+    ggplot(data=data$table, aes(x = Sepal.Length, y = Sepal.Width)) + 
+      geom_point(aes(color=Species, shape=Species)) +
+      xlab("Sepal Length") +  ylab("Sepal Width") +
+      ggtitle("Sepal Length-Width (ggplot2)")
+  })
+  
+  output$plotAvecPlotly <- renderPlotly({
+    if(!is.null(data$table)){
+      plot_ly(data = data$table, x = ~Petal.Length, y = ~Petal.Width, color = ~Species)%>%
+        layout(title = 'Petal Length-Width (ggplot2)',
+               yaxis = list(title = "Petal width"),
+               xaxis = list(title = "Petal length"))
+    }else {
+      NULL
+    }
+
+  })
+  
+  output$plotAvecGoogle <- renderGvis({
+    if(!is.null(data$table)){
+      gvisHistogram(as.data.frame(data$table$Petal.Width),
+                    options=list(title ="Petal width (Google)",
+                                 height=400)
+                    )
+    }else {
+      NULL
+    }
+    
+  })
+  
   
 }
 
